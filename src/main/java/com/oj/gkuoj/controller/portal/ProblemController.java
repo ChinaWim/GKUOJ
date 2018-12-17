e8914d44e744ace932e1227223c09b5e446f7685
MingX1 <969130721@qq.com>
2018-12-17 12:19:57 +0800

    update CompetitionProblem table

---
+++ src/main/java/com/oj/gkuoj/controller/portal/ProblemController.java
@@ -0,0 +1,23 @@
+package com.oj.gkuoj.controller.portal;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author m969130721@163.com
+ * @date 18-12-17 上午10:37
+ */
+@Controller
+public class ProblemController {
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/main/java/com/oj/gkuoj/entity/CompetitionProblem.java
+++ src/main/java/com/oj/gkuoj/entity/CompetitionProblem.java
@@ -6,6 +6,8 @@ import java.util.Date;
 public class CompetitionProblem implements Serializable {
     private Integer id;
 
+    private Integer compId;
+
     private Integer score;
 
     private Integer problemId;
@@ -14,8 +16,9 @@ public class CompetitionProblem implements Serializable {
 
     private Date updateTime;
 
-    public CompetitionProblem(Integer id, Integer score, Integer problemId, Date createTime, Date updateTime) {
+    public CompetitionProblem(Integer id, Integer compId, Integer score, Integer problemId, Date createTime, Date updateTime) {
         this.id = id;
+        this.compId = compId;
         this.score = score;
         this.problemId = problemId;
         this.createTime = createTime;
@@ -32,6 +35,14 @@ public class CompetitionProblem implements Serializable {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public Integer getCompId() {
+        return compId;
+    }
+
+    public void setCompId(Integer compId) {
+        this.compId = compId;
     }
 
     public Integer getScore() {
@@ -70,6 +81,7 @@ public class CompetitionProblem implements Serializable {
     public String toString() {
         return "CompetitionProblem{" +
                 "id=" + id +
+                ", compId=" + compId +
                 ", score=" + score +
                 ", problemId=" + problemId +
                 ", createTime=" + createTime +
