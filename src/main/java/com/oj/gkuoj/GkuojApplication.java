a65f07f8b248c041431d954b0c4c6317141650b3
MingX1 <969130721@qq.com>
2018-12-21 09:12:40 +0800

    add column tag

--- src/main/java/com/oj/gkuoj/GkuojApplication.java
+++ src/main/java/com/oj/gkuoj/GkuojApplication.java
@@ -1,9 +1,13 @@
 package com.oj.gkuoj;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan("com.oj.gkuoj.dao")
+@EnableTransactionManagement
 public class GkuojApplication {
 
     public static void main(String[] args) {
